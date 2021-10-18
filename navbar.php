8a06a4c9254c13ac9b0f63b6dbdf62c7b9c3167e
Nishant Acharekar <84176040+NishantA9@users.noreply.github.com>
2021-10-18 20:04:26 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,22 @@
+<!-- navbar --> 
+      <nav class="navbar navbar-expand-md navbar-dark bg-dark" style="background-color:black" >
+      <a class="navbar-brand" style="color:#2cff2c" href="index.php">SPARKS BANK</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto text-light">
+              <li class="nav-item">
+                <a class="nav-link" style="color:#2cff2c" href="index.php">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" style="color:#2cff2c" href="createuser.php">Create User</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" style="color:#2cff2c" href="transfermoney.php">Transfer Money</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" style="color:#2cff2c" href="transactionhistory.php">Transaction History</a>
+              </li>
+          </div>
+       </nav>
